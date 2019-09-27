0833357b06ec91870180e881d1378c7fa4facd33
Rejanezy <51627321+Rejanezy@users.noreply.github.com>
2019-09-27 22:37:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -62,47 +62,89 @@ var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	theme: "light2",
 	title:{
-		text: "Simple Line Chart IOT"
+		text: "Site Traffic"
 	},
-	axisY:{
-		includeZero: false
+	axisX:{
+		valueFormatString: "DD MMM",
+		crosshair: {
+			enabled: true,
+			snapToDataPoint: true
+		}
 	},
-	data: [{        
-		type: "line",       
+	axisY: {
+		title: "Number of Visits",
+		crosshair: {
+			enabled: true
+		}
+	},
+	toolTip:{
+		shared:true
+	},  
+	legend:{
+		cursor:"pointer",
+		verticalAlign: "bottom",
+		horizontalAlign: "left",
+		dockInsidePlotArea: true,
+		itemclick: toogleDataSeries
+	},
+	data: [{
+		type: "line",
+		showInLegend: true,
+		name: "Total Visit",
+		markerType: "square",
+		xValueFormatString: "DD MMM, YYYY",
+		color: "#F08080",
 		dataPoints: [
-			{ y: 0 },
-			{ y: 25},
-			{ y: 50, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
-			{ y: 55 },
-			{ y: 60 },
-			{ y: 65 },
-			{ y: 70 },
-			{ y: 75 },
-			{ y: 80 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-			{ y: 85 },
-			{ y: 90 },
-			{ y: 100 }
+			{ x: new Date(2017, 0, 3), y: 650 },
+			{ x: new Date(2017, 0, 4), y: 700 },
+			{ x: new Date(2017, 0, 5), y: 710 },
+			{ x: new Date(2017, 0, 6), y: 658 },
+			{ x: new Date(2017, 0, 7), y: 734 },
+			{ x: new Date(2017, 0, 8), y: 963 },
+			{ x: new Date(2017, 0, 9), y: 847 },
+			{ x: new Date(2017, 0, 10), y: 853 },
+			{ x: new Date(2017, 0, 11), y: 869 },
+			{ x: new Date(2017, 0, 12), y: 943 },
+			{ x: new Date(2017, 0, 13), y: 970 },
+			{ x: new Date(2017, 0, 14), y: 869 },
+			{ x: new Date(2017, 0, 15), y: 890 },
+			{ x: new Date(2017, 0, 16), y: 930 }
 		]
-	}]
-	[{        
-		type: "line",       
+	},
+	{
+		type: "line",
+		showInLegend: true,
+		name: "Unique Visit",
+		lineDashType: "dash",
 		dataPoints: [
-			{ y: 0 },
-			{ y: 25},
-			{ y: 50, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
-			{ y: 55 },
-			{ y: 60 },
-			{ y: 65 },
-			{ y: 70 },
-			{ y: 75 },
-			{ y: 80 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-			{ y: 85 },
-			{ y: 90 },
-			{ y: 100 }
+			{ x: new Date(2017, 0, 3), y: 510 },
+			{ x: new Date(2017, 0, 4), y: 560 },
+			{ x: new Date(2017, 0, 5), y: 540 },
+			{ x: new Date(2017, 0, 6), y: 558 },
+			{ x: new Date(2017, 0, 7), y: 544 },
+			{ x: new Date(2017, 0, 8), y: 693 },
+			{ x: new Date(2017, 0, 9), y: 657 },
+			{ x: new Date(2017, 0, 10), y: 663 },
+			{ x: new Date(2017, 0, 11), y: 639 },
+			{ x: new Date(2017, 0, 12), y: 673 },
+			{ x: new Date(2017, 0, 13), y: 660 },
+			{ x: new Date(2017, 0, 14), y: 562 },
+			{ x: new Date(2017, 0, 15), y: 643 },
+			{ x: new Date(2017, 0, 16), y: 570 }
 		]
 	}]
 });
 chart.render();
+
+function toogleDataSeries(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	} else{
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
+
 }
 </script>
 </head>
