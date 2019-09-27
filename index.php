3b06b79a2454c23a4358cd73aac03d313ed7b94f
Rejanezy <51627321+Rejanezy@users.noreply.github.com>
2019-09-27 22:43:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -94,21 +94,21 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		markerType: "square",
 		xValueFormatString: "DD MMM, YYYY",
 		color: "#F08080",
-		dataPoints:feeds:  [
-			{ x: new Date(2017, 0, 3), y: 510 },
-			{ x: new Date(2017, 0, 4), y: 560 },
-			{ x: new Date(2017, 0, 5), y: 540 },
-			{ x: new Date(2017, 0, 6), y: 558 },
-			{ x: new Date(2017, 0, 7), y: 544 },
-			{ x: new Date(2017, 0, 8), y: 693 },
-			{ x: new Date(2017, 0, 9), y: 657 },
-			{ x: new Date(2017, 0, 10), y: 663 },
-			{ x: new Date(2017, 0, 11), y: 639 },
-			{ x: new Date(2017, 0, 12), y: 673 },
-			{ x: new Date(2017, 0, 13), y: 660 },
-			{ x: new Date(2017, 0, 14), y: 562 },
-			{ x: new Date(2017, 0, 15), y: 643 },
-			{ x: new Date(2017, 0, 16), y: 570 }
+		dataPoints: [
+			{ x: new Date(2017, 0, 3), y: 650 },
+			{ x: new Date(2017, 0, 4), y: 700 },
+			{ x: new Date(2017, 0, 5), y: 710 },
+			{ x: new Date(2017, 0, 6), y: 658 },
+			{ x: new Date(2017, 0, 7), y: 734 },
+			{ x: new Date(2017, 0, 8), y: 963 },
+			{ x: new Date(2017, 0, 9), y: 847 },
+			{ x: new Date(2017, 0, 10), y: 853 },
+			{ x: new Date(2017, 0, 11), y: 869 },
+			{ x: new Date(2017, 0, 12), y: 943 },
+			{ x: new Date(2017, 0, 13), y: 970 },
+			{ x: new Date(2017, 0, 14), y: 869 },
+			{ x: new Date(2017, 0, 15), y: 890 },
+			{ x: new Date(2017, 0, 16), y: 930 }
 		]
 	},
 	{
