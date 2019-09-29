16d62849772b0408959b3dba3b68e62ae6a08e5a
Rejanezy <51627321+Rejanezy@users.noreply.github.com>
2019-09-29 18:24:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,63 +1,27 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
+<!DOCTYPE html>
+<html>
+<head>
+
+	 <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, LADDAWAN!</h1>
-    <button id="btnClick"> click </button>
-    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-  </body>
-//   <script>
-//  <script>
-      
-//       function plot (dataPoints) {
-    
-//     var chart = new CanvasJS.Chart("chartContainer", {
-//       animationEnabled: true,
-//       theme: "light2",
-//       title:{
-//         text: "Simple Line Chart"
-//       },
-//       axisY:{
-//         includeZero: false
-//       },
-//       data: [{        
-//         type: "line",       
-//         dataPoints: dataPoints
-//       }]
-//     });
-//     chart.render();
-    
-//     }
-//     $(()=>{
-//         $("button").click(function(){
-//             let urlAPI = "https://thingspeak.com/channels/860695/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15";
-//             $.get(urlAPI, function(data, status){
-//                // alert("Data: " + data + "\nStatus: " + status);
-//                 console.log(JSON.stringify(data.feeds));
-//                 var dataPoints = [];
-//                 plot(dataPoints);
-//             });
-//         });
-//       });  
-// </script>
-	<!DOCTYPE HTML>
-<html>
-<head>  
-<script>
- function plot () {
+</head>
+<body>
+ <h1>Hello, LADDAWAN!</h1>
+    <button id="btnClick"> click </button>
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+</body>
+<script type="text/javascript">
+windows.load=function () {
 var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	theme: "light2",
@@ -145,11 +109,4 @@ function toogleDataSeries(e){
 }
 }
 </script>
-</head>
-<!-- <body>
-<div id="chartContainer" style="height: 300px; width: 100%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-<button id="btnClick"> click </button>
-</body>
-</html> -->
 </html>
